5aef255549f76e6d2a128e597de4d99483c0e00b
KseniyaAnd <kseniya.andrusenko72@gmail.com>
2023-09-09 22:01:36 +0300

    init: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const bracketParis = {};
+
+  for (const pair of bracketsConfig){
+    bracketParis[paris[1]]=pair[0];
+  }
+
+  for (const char of str){
+    if (bracketParis[char] !== undefined){
+      const expectedBracket = bracketParis[cher];
+      if (stack.length === 0 || stack.pop() !== expectedBracket){
+        return false;
+      }
+    } else {
+      stack.push(char);
+    }
+  }
+
+  return stack.length === 0;
 }
